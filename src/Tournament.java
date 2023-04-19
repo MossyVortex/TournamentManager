23df1c527db36c61d0a848cd14689ab970c0bdbf
7ussainLZ <hussainlutfi@hotmail.com>
2023-04-19 06:12:08 +0300

    Part of Tournament Class

--- src/Tournament.java
+++ src/Tournament.java
@@ -1,3 +1,18 @@
-public class Tournament {
-    
+import java.util.ArrayList;
+import java.util.Date;
+
+public abstract class Tournament {
+    private String name, type, gameType, tournamentID, winner;
+    private Date startingDate, endingDate;
+    private ArrayList<Team> teams; private Boolean tournamentRegistrationStatus;
+    private int numOfTeams;
+
+//    public ArrayList<Team> getTeams() {
+//        return teams;
+//    }
+
+    public  Date[] getDates() {
+        Date[] dates = {startingDate,endingDate};
+        return dates;
+    }
 }
