c54ce7b5bd122f3ae2453dd724c971469e5beaaf
ma278hdi <monday22220@gmail.com>
2023-04-19 21:16:11 +0300

    I add the essantial methods for person student admin and team

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,30 @@
 public class Person {
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private int ID;
+    private String password;
     
+    public Person(){
+
+    }
+    
+    private String generateID(int ID){
+
+    }
+    public String getName() {
+        return name;
+    }
+    public int getID() {
+        return ID;
+    } 
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
