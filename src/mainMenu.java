9e5967710e84e585c7d80f457f5bee45d8627358
MOSA <mosa20111430@gmail.com>
2023-04-20 04:05:32 +0300

    if statement for create tournament

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -6,28 +6,57 @@ public class mainMenu {
         
         //JAVAFX starting point
 
-        // login page
+        String accountType = "guest";
+        // login page or register new account page
+
+        // after login update accountType appropriately
 
         // create a tornament
+        createTournament(accountType);
 
-        String type = "";
-        // choose tournament type page
+    }
 
-        boolean flag = false;
-        Scanner input = new Scanner(System.in);
+    public static void createTournament(String accountType){
 
-        while(!flag){
+        if(accountType.equals("Admin")){
 
-            type = input.nextLine();
-            type.toLowerCase();
-            if(type.equals("roundrobin")){
-                System.out.println("tournament name: ");
-                
-                // new RoundRobin(, type, type, type, null, null, null, 0, args, null)
-            }
-            else if(type.equals("elimination")){
-                // to be continued
+            String tournamentType = "";
+            // choose tournament type page
+
+            boolean flag = false;
+            Scanner input = new Scanner(System.in);
+
+            while(!flag){
+
+                System.out.println("choose Touranment Type: ");
+                tournamentType = input.nextLine();
+                tournamentType.toLowerCase();
+
+                System.out.println("choose Touranment name: ");
+                String tournamnetName = input.nextLine();
+
+                System.out.println("what game will you be playing?: ");
+                String gameType = input.nextLine();
+
+                String tournamnetID = generateTournamentID(1); // 1 is a placeholder it should be tournament number which will be counted continously and saved in a txt file
+
+                System.out.println("how many teams will play?: ");
+                int numOfTeams = input.nextInt();
+
+
+                if(tournamentType.equals("roundrobin")){
+                    System.out.println("tournament name: ");
+                    
+                    new RoundRobin(tournamnetName, gameType, tournamnetName, tournamnetName, null, null, null, numOfTeams, null, null);
+                }
+                else if(tournamentType.equals("elimination")){
+                    new Elimination(tournamnetName, gameType, tournamnetName, tournamnetName, null, null, null, numOfTeams, null, null);
+                }
             }
         }
     }
+
+    public static String generateTournamentID(int numOfTournamnets){
+        return "2023" + numOfTournamnets+1;
+    } 
 }
