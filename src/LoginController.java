ff9189c6f888c77b3eb0691a8be5d42fe0af63d2
7ussainLZ <hussainlutfi@hotmail.com>
2023-04-22 09:10:56 +0300

    JFX Register

---
+++ src/LoginController.java
@@ -0,0 +1,45 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private ImageView KFUPMIcon;
+
+    @FXML
+    private Label emailLable;
+
+    @FXML
+    private TextField emailTextF;
+
+    @FXML
+    private Label existingLable;
+
+    @FXML
+    private ImageView iconLogo;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginLable;
+
+    @FXML
+    private Label passwordLable;
+
+    @FXML
+    private TextField passwordTextF;
+
+    @FXML
+    private Label registerLable;
+
+    @FXML
+    void registerLabelOnClicked(MouseEvent event) {
+
+    }
+
+}
--- src/TournamentApplication.java
+++ src/TournamentApplication.java
@@ -1,5 +1,11 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class TournamentApplication extends Application {
 
@@ -8,7 +14,13 @@ public class TournamentApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+            Parent fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("LoginScene.fxml")));
+            Scene loginPage = new Scene(fxmlLoader);
+            primaryStage.setScene(loginPage);
+            primaryStage.setTitle("Tournament Manager - Login");
+            primaryStage.show();
     }
+
+
 }
