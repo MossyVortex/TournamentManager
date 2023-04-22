7a3db751d60662942c1d9a70f3574cbf1cac0c44
7ussainLZ <hussainlutfi@hotmail.com>
2023-04-22 09:38:59 +0300

    JFX Logic

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,9 +1,16 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
 
@@ -39,7 +46,17 @@ public class LoginController {
 
     @FXML
     void registerLabelOnClicked(MouseEvent event) {
-
+//        Parent fxmlLoader = null;
+//        try {
+//            fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("RegesterScene.fxml")));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Scene registerPage = new Scene(fxmlLoader);
+//        Stage stage = new Stage();
+//        stage.setScene(registerPage);
+//        stage.setTitle("Tournament Manager - Register");
+//        stage.show();
     }
 
 }
