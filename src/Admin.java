94e0b7ee66cd88487c6800fd446e06cffcaae62d
7ussainLZ <hussainlutfi@hotmail.com>
2023-04-25 18:17:17 +0300

    JFX Logic 5

--- src/Admin.java
+++ src/Admin.java
@@ -3,21 +3,14 @@ public class Admin extends Person{
     private boolean IsAuthorized;
     private String club;
 
-    public Admin(String name, String phoneNumber, String email, int ID, String password,int tournamentsCreated,boolean IsAuthorized,String club){
+    public Admin(String name, String phoneNumber, String email, int ID, String password,int tournamentsCreated,boolean IsAuthorized){
         super(name,phoneNumber,email,ID,password);
         this.tournamentsCreated = tournamentsCreated;
-        this.IsAuthorized = IsAuthorized;
-        this.club = club;
     }
-     public String getClub() {
-         return club;
-     }
      public int getTournamentsCreated() {
          return tournamentsCreated;
      }
      public boolean getIsAuthorized() {
          return IsAuthorized;
-     } 
-     
-     
+     }
 }
--- src/RegisterStudentController.java
+++ src/RegisterStudentController.java
@@ -8,19 +8,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 
-public class RegisterAdminController {
+public class RegisterStudentController {
 
     @FXML
     private ImageView backButton;
@@ -30,6 +28,12 @@ public class RegisterAdminController {
 
     @FXML
     private TextField emailTextField;
+
+    @FXML
+    private Label heightLable;
+
+    @FXML
+    private TextField heightTextField;
 
     @FXML
     private Label nameLable;
@@ -50,29 +54,47 @@ public class RegisterAdminController {
     private TextField phoneTextField;
 
     @FXML
-    private Button regesterButton;
+    private Button registerButton;
 
     @FXML
-    void regesterButtonOnclicked(ActionEvent event) {
+    private Label weightLable;
+
+    @FXML
+    private TextField weightTextField;
+
+    @FXML
+    void backButtonOnClicked(KeyEvent event) {
+        Parent fxmlLoader = null;
+        try {
+            fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("RegesterScene.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene loginStudentPage = new Scene(fxmlLoader);
+        Stage stage = (Stage)(((Node)event.getSource()).getScene().getWindow()) ;
+        stage.setScene(loginStudentPage);
+        stage.setTitle("Tournament Manager - Register");
+        stage.show();
+    }
+
+    @FXML
+    void registerButtonOnClicked(ActionEvent event) {
+
         if (allFilled()){}
-//            try {
-//                File file = new File("AdminsFile.csv");
-//
-//            }
-//            catch (IOException e){
-//                System.out.println(e.getCause());
-//            }
-//        }
+
     }
 
     public boolean allFilled(){
         boolean allFilled = true;
         nameLable.setTextFill(Paint.valueOf("#386641")); phoneLable.setTextFill(Paint.valueOf("#386641"));
         passwordLable.setTextFill(Paint.valueOf("#386641")); emailLable.setTextFill(Paint.valueOf("#386641"));
+        weightLable.setTextFill(Paint.valueOf("#386641")); heightLable.setTextFill(Paint.valueOf("#386641"));
         nameTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
         emailTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
         passwordTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
         phoneTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
+        weightTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
+        heightTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#386641"), BorderStrokeStyle.SOLID,null,null)));
 
         if (nameTextField.getText().isEmpty()){
             nameLable.setTextFill(Paint.valueOf("#BC4749"));
@@ -94,23 +116,17 @@ public class RegisterAdminController {
             phoneTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#BC4749"), BorderStrokeStyle.SOLID,null,null)));
             allFilled = false;
         }
+        if (weightTextField.getText().isEmpty()){
+            weightLable.setTextFill(Paint.valueOf("#BC4749"));
+            weightTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#BC4749"), BorderStrokeStyle.SOLID,null,null)));
+            allFilled = false;
+        }
+        if (heightTextField.getText().isEmpty()){
+            heightLable.setTextFill(Paint.valueOf("#BC4749"));
+            heightTextField.setBorder(new Border(new BorderStroke(Paint.valueOf("#BC4749"), BorderStrokeStyle.SOLID,null,null)));
+            allFilled = false;
+        }
+
         return allFilled;
     }
-
-    @FXML
-    void setBackButton(MouseEvent event) {
-        Parent fxmlLoader = null;
-        try {
-            fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("RegesterScene.fxml")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene loginStudentPage = new Scene(fxmlLoader);
-        Stage stage = (Stage)(((Node)event.getSource()).getScene().getWindow()) ;
-        stage.setScene(loginStudentPage);
-        stage.setTitle("Tournament Manager - Register");
-        stage.show();
-    }
-
-
 }
