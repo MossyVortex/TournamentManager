0731c91266247a0f301d2ef060e589a13ed781f1
ma278hdi <monday22220@gmail.com>
2023-04-27 20:30:04 +0300

    I complete the login controller

--- src/LoginController.java
+++ src/LoginController.java
@@ -18,7 +18,6 @@ import java.util.Objects;
 
 public class LoginController {
 
-
     @FXML
     private ImageView KFUPMIcon;
 
@@ -67,7 +66,7 @@ public class LoginController {
             e.printStackTrace();
         }
         Scene registerPage = new Scene(fxmlLoader);
-        Stage stage = (Stage)(((Node)event.getSource()).getScene().getWindow()) ;
+        Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
         stage.setScene(registerPage);
         stage.setTitle("Tournament Manager - Register");
         stage.show();
@@ -94,10 +93,54 @@ public class LoginController {
         if(studentRadioButton.isSelected()){
             FileReader student = new FileReader("StudentsFile.csv");
             BufferedReader readStudentFile = new BufferedReader(student);
-            while(readStudentFile.readLine() != null){
-                
+            String line = readStudentFile.readLine();
+            while(line != null){
+                String[] valuesOfStudentFile = line.split(",");
+                if(valuesOfStudentFile[3].equals(emailTextF.getText())){
+                    if(valuesOfStudentFile[4].equals(passwordTextF.getText())){
+                        Parent fxmlLoader = null;
+                    try {
+                         fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomeScene.fxml")));
+                    } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Scene homePage = new Scene(fxmlLoader);
+                        Stage stage = (Stage)(((Node)event.getSource()).getScene().getWindow()) ;
+                        stage.setScene(homePage);
+                        stage.setTitle("Tournament Manager - Home Page");
+                        stage.show();
+                    }
+                        
+                }
             }
+        System.out.println("One of the email or password is/are not correct");
+        }else if(adminRadioButton.isSelected()){
+            FileReader Admin = new FileReader("AdminsFile.csv");
+            BufferedReader readAdmins = new BufferedReader(Admin);
+            String line = readAdmins.readLine();
+            while(line != null){
+                String[] valuesOfAdminFile = line.split(",");
+                if(valuesOfAdminFile[3].equals(emailTextF.getText())){
+                    if(valuesOfAdminFile[4].equals(passwordTextF.getText())){
+                        Parent fxmlLoader = null;
+                    try {
+                         fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomeScene.fxml")));
+                    } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Scene homePage = new Scene(fxmlLoader);
+                        Stage stage = (Stage)(((Node)event.getSource()).getScene().getWindow()) ;
+                        stage.setScene(homePage);
+                        stage.setTitle("Tournament Manager - Home Page");
+                        stage.show();
+                    }
+                }
+
+            }
+            System.out.println("One of the email or password is/are not correct");
         }
+       }else{
+        System.out.println("fill the blank");
        }
 
     }
