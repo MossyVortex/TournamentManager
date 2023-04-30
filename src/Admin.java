419094f17b8487c93559b27aa5c12a575ff83ae3
7ussainLZ <hussainlutfi@hotmail.com>
2023-04-30 20:40:13 +0300

    Home Scenes with Login

--- src/Admin.java
+++ src/Admin.java
@@ -6,6 +6,7 @@ public class Admin extends Person{
     public Admin(String name, String phoneNumber, String email, String ID, String password,int tournamentsCreated,boolean IsAuthorized){
         super(name,phoneNumber,email,ID,password);
         this.tournamentsCreated = tournamentsCreated;
+        this.IsAuthorized = IsAuthorized;
     }
      public int getTournamentsCreated() {
          return tournamentsCreated;
