b73385233cfb03e57e292c54cc41696daea737df
7ussainLZ <hussainlutfi@hotmail.com>
2023-05-05 01:01:44 +0300

    ViewTournament

---
+++ src/ViewTournamentController.java
@@ -0,0 +1,101 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class ViewTournamentController {
+
+    @FXML
+    private ImageView EditButton;
+
+    @FXML
+    private Label IDLabel;
+
+    @FXML
+    private Label StatusLabel;
+
+    @FXML
+    private TextField StatusTF;
+
+    @FXML
+    private TextField WinnerTextField;
+
+    @FXML
+    private Label WinsLabel;
+
+    @FXML
+    private Label WinsLabel1;
+
+    @FXML
+    private Label WinsLabel2;
+
+    @FXML
+    private ImageView backButton;
+
+    @FXML
+    private Label emailLabel;
+
+    @FXML
+    private DatePicker endingDateDatePicker;
+
+    @FXML
+    private TextField gameTextField;
+
+    @FXML
+    private Label nameLable;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private Label passwordLable;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private DatePicker startingDateDatePicker;
+
+    @FXML
+    private TextField stdNumTextField;
+
+    @FXML
+    private HBox studentsPane;
+
+    @FXML
+    private TextField teamsNumTextField;
+
+    @FXML
+    private HBox teamsPane;
+
+    @FXML
+    void editButtonOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setBackButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void studentsPaneOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void teamsPaneOnCklicked(MouseEvent event) {
+
+    }
+
+}
