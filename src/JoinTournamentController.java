b171f05d162c3a66d87ebdc9a06d23ea8fdc1535
7ussainLZ <hussainlutfi@hotmail.com>
2023-05-04 22:17:07 +0300

    JoinTournamentScene+Controllers

--- src/JoinTournamentController.java
+++ src/JoinTournamentController.java
@@ -2,21 +2,68 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class JoinTournamentController {
 
     @FXML
-    private Label enterIDLable;
+    private ImageView backTIDButton;
 
     @FXML
-    private TextField enterIDTextField;
+    private RadioButton individualRB;
 
     @FXML
     private Button joinButton;
 
     @FXML
+    private RadioButton teamRB;
+
+    @FXML
+    private TextField tournamentIDTextField;
+
+    @FXML
+    private ImageView backTeamJoinButton;
+
+    @FXML
+    private Button enterButton;
+
+
+    @FXML
+    private TextField memberIDTextField;
+
+    @FXML
+    private Label remaningNumLable;
+
+    @FXML
+    void enterButtonOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setTeamBackButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void individualRBOn(ActionEvent event) {
+
+    }
+
+    @FXML
     void joinButtonOnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setTIDBackButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void teamRBOn(ActionEvent event) {
 
     }
 
